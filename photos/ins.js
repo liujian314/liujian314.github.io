1acfd689e491b6b94113b7081372c6a1d0087f23
liujian314 <liujian314@hotmail.com>
2017-10-30 23:23:11 +0800

    Site updated: 2017-10-30 23:23:07

--- photos/ins.js
+++ photos/ins.js
@@ -125,16 +125,16 @@
   
             liTmpl += '<figure class="thumb" itemprop="associatedMedia" itemscope="" itemtype="http://schema.org/ImageObject">\
                   <a href="' + src + '" itemprop="contentUrl" data-size="1080x1080" data-type="' + type + '" data-target="' + src + '">\
-                    <img class="reward-img" data-type="' + type + '" data-src="' + src + '" src="./assets/empty.jpeg" itemprop="thumbnail" onload="lzld(this)">\
+                    <img class="reward-img" data-type="' + type + '" data-src="' + src + '" src="./assets/empty.png" itemprop="thumbnail" onload="lzld(this)">\
                   </a>\
                   <figcaption style="display:none" itemprop="caption description">' + data.text[i] + '</figcaption>\
               </figure>';
           }
-          ulTmpl = ulTmpl + '<section class="archives album"><h1 class="year">' + data.year + '年<em>' + data.month + '月</em></h1>\
+          ulTmpl = ulTmpl + '<section class="archives album post-header"><h1 class="year">' + data.year + '年<em>' + data.month + '月</em></h1>\
           <ul class="img-box-ul">' + liTmpl + '</ul>\
           </section>';
         }
-        document.querySelector('.instagram').innerHTML = '<div class="photos" itemscope="" itemtype="http://schema.org/ImageGallery">' + ulTmpl + '</div>';
+        document.querySelector('.instagram').innerHTML = '<div class="photos posts-collapse" itemscope="" itemtype="http://schema.org/ImageGallery">' + ulTmpl + '</div>';
         createVideoIncon();
         _view2.default.init();
       };
